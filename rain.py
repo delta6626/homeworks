8fa33ee18ae00bbba6ff244e05cbf656f6ca5c78
= <=>
2022-02-19 14:22:56 +0400

    Rainy day

---
+++ rain.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+characters = "|"
+space = " "
+show = True
+
+def rain():
+    time.sleep(0.1)
+    print((characters+space*random.randint(5,10))*random.randint(10,20))
+
+while(show):
+    rain()
